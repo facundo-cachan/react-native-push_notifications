02f9850f5e1cab848f8a03178f46ba7d08f90854
Facundo Cachan <facundo.cachan.ext@digitalfemsa.com>
2023-12-16 23:58:27 -0300

    chore(code) :tada: First commit

---
+++ ios/PushNotification/AppDelegate.mm
@@ -0,0 +1,25 @@
+#import "AppDelegate.h"
+#import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"PushNotification";
+  [FIRApp configure];
+  self.initialProps = @{};
+
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
